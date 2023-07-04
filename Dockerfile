1f21239dbbb854a5e298420d94c0276b57ae2157
lhwdev <lhwdev6@outlook.com>
2023-07-04 22:22:00 +0900

    e

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM node
-
-COPY server /usr/share/website-test
-COPY public /usr/share/http-public
--- server/src/index.ts
+++
@@ -1,5 +0,0 @@
-import server from "http-server";
-
-server.createServer({
-  root: "/usr/share/http-public",
-}).listen(80);
